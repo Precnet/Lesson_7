d0864f199187df76209d6a701ee15e63a84ca5fa
tester <dmfrodol@yandex.ru>
2019-11-21 05:11:49 -0800

    add requirements to passenger carriage

--- passenger_carriage.rb
+++ passenger_carriage.rb
@@ -1,5 +1,6 @@
 require_relative 'manufacturer.rb'
 require_relative 'carriage.rb'
+require_relative 'railway_error.rb'
 
 class PassengerCarriage < Carriage
   CARRIAGE_TYPE = 'passenger'.freeze
@@ -7,6 +8,27 @@ class PassengerCarriage < Carriage
   def initialize(number = generate_number(LENGTH), num_of_seats)
     super number
     @type = CARRIAGE_TYPE
-    @num_of_seats = num_of_seats
+    @number_of_seats = Hash.new
+    @number_of_seats[:total] = num_of_seats
+    @number_of_seats[:taken] = 0
+  end
+
+  def take_seat
+    no_seats_error = "There are no empty seats in carriage '#{number}'"
+    raise RailwayError, no_seats_error unless free_seats?
+    @number_of_seats[:taken] += 1
+  end
+
+  def number_of_seats
+    @number_of_seats[:total]
+  end
+
+  def free_seats
+    @number_of_seats[:total] - @number_of_seats[:taken]
+  end
+
+  private
+  def free_seats?
+    free_seats > 0
   end
 end
