d819126a4a3f8f58660c2f7f9376a35d32f8c5c5
tester <dmfrodol@yandex.ru>
2019-11-21 00:32:30 -0800

    add num_of_seats parameter

--- passenger_carriage.rb
+++ passenger_carriage.rb
@@ -4,8 +4,9 @@ require_relative 'carriage.rb'
 class PassengerCarriage < Carriage
   CARRIAGE_TYPE = 'passenger'.freeze
 
-  def initialize(number = generate_number(LENGTH))
+  def initialize(number = generate_number(LENGTH), num_of_seats)
     super number
     @type = CARRIAGE_TYPE
+    @num_of_seats = num_of_seats
   end
 end
