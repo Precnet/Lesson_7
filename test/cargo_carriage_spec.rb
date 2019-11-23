e030f6477a4b1ca7943b295e1a087a1484f7689a
Dmitry Davdov <dmfrodol@yandex.ru>
2019-11-23 19:12:40 +0300

    add support for volume of cargo carriages fix error with wrong class

--- test/cargo_carriage_spec.rb
+++ test/cargo_carriage_spec.rb
@@ -3,7 +3,7 @@ require_relative '../cargo_carriage.rb'
 
 describe 'CargoCarriage' do
   before(:all) do
-    @carriage = CargoCarriage.new
+    @carriage = CargoCarriage.new(10)
   end
   it 'should create cargo carriage' do
     expect(@carriage.type).to eq('cargo')
@@ -13,7 +13,7 @@ describe 'CargoCarriage' do
     expect(@carriage.manufacturer).to eq('Train inc.')
   end
   it 'should create carriages with fixed number' do
-    carriage2 = PassengerCarriage.new('1234', 10)
+    carriage2 = CargoCarriage.new('1234', 10)
     expect(carriage2.number).to eq('1234')
   end
 end
