dfd02ffbefa6996ce1ebf04b1468f317a4346363
Dmitry Davdov <dmfrodol@yandex.ru>
2019-11-23 19:03:14 +0300

    add new attribute add require for error

--- cargo_carriage.rb
+++ cargo_carriage.rb
@@ -1,11 +1,20 @@
 require_relative 'manufacturer.rb'
 require_relative 'carriage.rb'
+require_relative 'railway_error.rb'
 
 class CargoCarriage < Carriage
   CARRIAGE_TYPE = 'cargo'
 
-  def initialize(carriage_number = generate_number(LENGTH))
+  def initialize(carriage_number = generate_number(LENGTH), max_cargo_volume)
     super carriage_number
     @type = CARRIAGE_TYPE
+    @volume = Hash.new
+    @volume[:max] = max_cargo_volume
+    @volume[:taken] = 0
+  end
+
+  def place_cargo(volume)
+    no_space_error = "Not enough space to place your cargo!"
+    raise RailwayError, no_space_error unless
   end
 end
