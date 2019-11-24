dd3a1c22610eb552652a0fb45e5dfc2cfd108da4
Dmitry Davdov <dmfrodol@yandex.ru>
2019-11-24 13:49:33 +0300

    add method each_train to module

--- train_iterator.rb
+++ train_iterator.rb
@@ -1,1 +1,10 @@
+require_relative 'railway_error.rb'
 
+module TrainIterator
+  def each_train
+    no_block_error = 'This method requires block to operate!'
+    raise RailwayError, no_block_error unless block_given?
+
+    @carriages.each{ |carriage| yield carriage }
+  end
+end
