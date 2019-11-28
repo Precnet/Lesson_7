619dd0b499e9e164597548756e72373715b7aba1
tester <dmfrodol@yandex.ru>
2019-11-28 06:28:48 -0800

    fix rubocop

--- route.rb
+++ route.rb
@@ -23,6 +23,7 @@ class Route
     # check for duplication
     duplicate_message = 'Can`t add duplicate stations to the route!'
     raise ArgumentError, duplicate_message if @stations.find_index(new_station)
+
     @stations.insert(-2, new_station)
   end
 
