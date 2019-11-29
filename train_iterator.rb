e8afecb44dc953c1e9402771a4fd7e7e5be48a9f
tester <dmfrodol@yandex.ru>
2019-11-28 23:35:10 -0800

    fix rubocop

--- train_iterator.rb
+++ train_iterator.rb
@@ -5,6 +5,6 @@ module TrainIterator
     no_block_error = 'This method requires block to operate!'
     raise RailwayError, no_block_error unless block_given?
 
-    @carriages.each{ |carriage| yield carriage }
+    @carriages.each { |carriage| yield carriage }
   end
 end
